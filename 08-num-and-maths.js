3723c54bd47fa80f1ab6d1118e921b1cc6ff9bcf
Kumari Anjali Rao <118367818+anjalirao2001@users.noreply.github.com>
2024-05-07 00:29:11 +0530

    added numbers and math in js

---
+++ 08-num-and-maths.js
@@ -0,0 +1,38 @@
+/*const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const a = 123.8936
+
+console.log(a.toPrecision(3));
+
+const hundred = 1000000
+console.log(hundred.toLocaleString('en-IN'));
+*/
+
+
+// *************Maths**********
+
+/*
+console.log(Math);
+console.log(Math.abs(-7));
+console.log(Math.round(7.9));
+console.log(Math.ceil(7.9));
+console.log(Math.floor(7.9));
+
+*/
+
+// console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+const random = Math.floor(Math.random() * (max - min + 1)) + min
+
+console.log(random);
